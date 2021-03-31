db0a43456f93fc52a101a93acb88be5453200d3c
Carlos <carlos.alfonso.montero@gmail.com>
2021-03-31 12:07:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    if (feet <= 400) {
+      return 'This one is on me!';
+    } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else if (feet > 2500) {
+      return 'No can do.';
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const returnMessage = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return returnMessage;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+      break;
+  }
 }
